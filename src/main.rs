08319434639018ac8fc9ec276c63da01946c30c1
Korben Tompkin <86434527+korbexmachina@users.noreply.github.com>
2023-11-22 00:10:49 -0800

    working on the new implementation, need to get response string out of the response object

--- src/main.rs
+++ src/main.rs
@@ -2,44 +2,46 @@
  * Run git diff and pass the output to an ollama model to generate a commit message
  */
 
+// Ignore unused warnings for now
+#[allow(unused_imports)]
+
 use {
     std::process::Command,
-    std::str,
+    http::{Request, Response},
+    std::collections::HashMap,
+    reqwest::{Client, Error},
+    json::JsonValue,
 };
 
-fn main() {
-    let prompt = "Summarize the changes outlined in this file:".to_string();
-    let model = "llama2".to_string();
-    let diff = get_diff();
-    let commit_message = get_commit_message(prompt, model, diff);
-    println!("{}", commit_message);
-}
 
-// Run git diff and return the output as a string
-fn get_diff() -> String {
+fn diff() -> String {
     let output = Command::new("git")
         .arg("diff")
         .output()
-        .expect("failed to execute process");
+        .expect("Failed to execute git diff");
 
-    let diff = str::from_utf8(&output.stdout)
-        .expect("failed to convert output to string");
+    let output = String::from_utf8(output.stdout).expect("Failed to parse git diff output");
 
-    return diff.to_string();
+    output
 }
 
-// Run ollama with the provided prompt, model, and diff and return the output as a string
-fn get_commit_message(prompt: String, model: String, diff: String) -> String {
-    let output = Command::new("ollama")
-        .arg("run")
-        .arg(model)
-        .arg(prompt)
-        .arg(diff)
-        .output()
-        .expect("failed to execute process");
+fn get_message() -> String {
+    // Make an http request to the ollama server on localhost:11434
 
-    let commit_message = str::from_utf8(&output.stdout)
-        .expect("failed to convert output to string");
+    // Format the diff output into a json object
+    let diff = diff();
+    let prompt = format!("Create a commit message for the following diff:\n{}", diff);
 
-    return commit_message.to_string();
-}
+    let mut map = HashMap::new();
+    map.insert("model", "llama2");
+    map.insert("prompt", &prompt);
+    map.insert("stream", "false");
+
+    let client = reqwest::Client::new();
+
+    // Send the request
+    let resp = client.post("http://127.0.0.1:11434")
+        .json(&map)
+        .send();
+
+} 
